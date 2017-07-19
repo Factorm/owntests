7195cf3e1666c743d0929679fb023dd678b34287
Factor <Factor@192.168.0.31>
2017-07-19 19:51:53 +0200

    # WARNING: head commit changed in the meantime
    2st commit

---
+++ src/main/java/com/expedia/owntests/App.java
@@ -0,0 +1,13 @@
+package com.expedia.owntests;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
