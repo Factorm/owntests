dc8327fcaebddd6a7712c6b454ab39cb10dbec5d
factor.loster <factor.loster@gmail.com>
2017-07-20 11:49:10 +0200

    com

---
+++ src/test/java/com/expedia/owntests/FlightsBookmark.java
@@ -0,0 +1,136 @@
+package com.expedia.owntests;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+
+import pageclasses.Roundtrip;
+import pageclasses.OneWay;
+
+public class FlightsBookmark {
+	private WebDriver driver;
+	private String baseUrl;
+	ExtentReports report;
+	ExtentTest test;
+	private Roundtrip rt;
+	private OneWay ow;
+
+	@BeforeClass
+	public void beforeClass() {
+		report = new ExtentReports("E://Development//FlightsTabTest.html");
+		test = report.startTest("Verify Welcome Text");
+
+		System.setProperty("webdriver.chrome.driver",
+				"E://Development//workspace - Mariusz//Selenium//chromedriver.exe");
+		
+
+	}
+
+	@AfterClass
+	public void afterClass() throws Exception {
+		test.log(LogStatus.PASS, "Test passed");
+		Thread.sleep(2000);
+		driver.quit();
+		test.log(LogStatus.INFO, "Browser Ended...");
+		report.endTest(test);
+		report.flush();
+	}
+
+	@BeforeMethod
+	public void beforeMethod() {
+
+		driver = new ChromeDriver();
+		
+		rt = new Roundtrip(driver, test);
+		ow = new OneWay(driver, test);
+
+		baseUrl = "https://www.expedia.com/";
+		test.log(LogStatus.INFO, "Browser Started...");
+
+		driver.manage().window().maximize();
+		test.log(LogStatus.INFO, "Window is maximize...");
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.get(baseUrl);
+		test.log(LogStatus.INFO, "Web application opened...");
+
+		rt.clickFlightTab();
+	}
+
+	@AfterMethod
+	public void afterMethod(){
+		driver.quit();
+	}
+	
+	@Test
+	public void RoundtripTest() {
+
+		rt.clickRoundtripTab();
+		rt.enterFlyingFrom("New York");
+		rt.enterFlyingTo("Chicago");
+		rt.enterDeparting("10/24/2017");
+		rt.enterReturning("10/30/2017");
+		rt.enterAdults("2");
+		rt.enterChildren("0");
+		rt.clickAdvancedOption();
+		rt.chosePreferredClass("Business");
+		rt.clickSearchButton();
+
+		WebElement verificationText = null;
+
+		try {
+			WebDriverWait wait = new WebDriverWait(driver, 5);
+
+			verificationText = wait
+					.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@class='title-city-text']")));
+		} catch (NoSuchElementException e) {
+			System.out.println(e.getMessage());
+		}
+
+		Assert.assertTrue(verificationText != null);
+		test.log(LogStatus.PASS, "Verifield verificationText");
+	}
+
+	@Test
+	public void oneWayTest() {
+
+		ow.clickoneWayTab();
+		ow.enterFlyingFrom("New York");
+		ow.enterFlyingTo("Chicago");
+		ow.enterDeparting("10/24/2017");
+		ow.enterAdults("2");
+		ow.enterChildren("0");
+		ow.clickAdvancedOption();
+		ow.chosePreferredClass("Business");
+		ow.clickSearchButton();
+
+		WebElement verificationText = null;
+
+		try {
+			WebDriverWait wait = new WebDriverWait(driver, 5);
+
+			verificationText = wait
+					.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@class='title-city-text']")));
+		} catch (NoSuchElementException e) {
+			System.out.println(e.getMessage());
+		}
+
+		Assert.assertTrue(verificationText != null);
+		test.log(LogStatus.PASS, "Verifield verificationText");
+	}
+}
---
+++ src/test/java/pageclasses/FlightTab.java
@@ -0,0 +1,90 @@
+package pageclasses;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+
+public class FlightTab {
+
+	ExtentTest test;
+	protected WebDriver driver = null;
+	// public static WebElement element = null;
+
+	public FlightTab(WebDriver driver, ExtentTest test) {
+		this.driver = driver;
+		this.test = test;
+	}
+
+	By flightTab = By.id("tab-flight-tab-hp");
+	By roundtripTab = By.id("flight-type-roundtrip-label-hp-flight");
+	By oneWayTab = By.id("flight-type-one-way-label-hp-flight");
+	By flyingFrom = By.id("flight-origin-hp-flight");
+	By flyingTo = By.id("flight-destination-hp-flight");
+	By adults = By.id("flight-adults-hp-flight");
+	By children = By.id("flight-children-hp-flight");
+	By advancedOptions = By.id("flight-advanced-options-hp-flight");
+	By preferredCLass = By.id("flight-advanced-preferred-class-hp-flight");
+	By searchButton = By.xpath("//section[@id='section-flight-tab-hp']//button[@type='submit']");
+
+	public void clickFlightTab() {
+		driver.findElement(flightTab).click();
+		test.log(LogStatus.INFO, "FlightTab clicked");
+	}
+
+	public void clickRoundtripTab() {
+		driver.findElement(roundtripTab).click();
+		test.log(LogStatus.INFO, "RoundtripTab clicked");
+	}
+
+	public void clickoneWayTab() {
+		driver.findElement(oneWayTab).click();
+		test.log(LogStatus.INFO, "OneWayTab clicked");
+	}
+
+	public void enterFlyingFrom(String textFrom) {
+		driver.findElement(flyingFrom).sendKeys(textFrom);
+		test.log(LogStatus.INFO, "To Flying From put: " + textFrom);
+	}
+
+	public void enterFlyingTo(String textTo) {
+		driver.findElement(flyingTo).sendKeys(textTo);
+		test.log(LogStatus.INFO, "To Flying To put: " + textTo);
+	}
+
+	public void enterAdults(String textAdults) {
+		driver.findElement(adults).sendKeys(textAdults);
+		test.log(LogStatus.INFO, "To Adults To put: " + textAdults);
+	}
+
+	public void enterChildren(String textChildren) {
+		driver.findElement(children).sendKeys(textChildren);
+		test.log(LogStatus.INFO, "To Children To put: " + textChildren);
+	}
+
+	public void clickAdvancedOption() {
+		driver.findElement(advancedOptions).click();
+		test.log(LogStatus.INFO, "Advanced option clicked");
+	}
+
+	public void chosePreferredClass(String chose) {
+		WebElement element = driver.findElement(preferredCLass);
+		List<WebElement> lists = element.findElements(By.tagName("option"));
+
+		for (WebElement list : lists) {
+			if (list.getText().equals(chose)) {
+				list.click();
+			}
+		}
+		test.log(LogStatus.INFO, "Preferred class chosed");
+	}
+
+	public void clickSearchButton() {
+		driver.findElement(searchButton).click();
+		test.log(LogStatus.INFO, "Search button clicked");
+	}
+}
